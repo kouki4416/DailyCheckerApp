5861d9234c18961bdf1371429b01cf39db97b92b
kouki4416 <pank4416@gmail.com>
2022-10-25 20:58:07 +0900

    Add functionality to count checked dates

--- app/src/main/java/com/pyunku/dailychecker/calendar/presentation/CalendarScreen.kt
+++ app/src/main/java/com/pyunku/dailychecker/calendar/presentation/CalendarScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,6 +28,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pyunku.dailychecker.R
 import io.github.boguszpawlowski.composecalendar.SelectableCalendar
 import io.github.boguszpawlowski.composecalendar.day.DayState
@@ -35,7 +37,6 @@ import io.github.boguszpawlowski.composecalendar.rememberSelectableCalendarState
 import io.github.boguszpawlowski.composecalendar.selection.DynamicSelectionState
 import io.github.boguszpawlowski.composecalendar.selection.SelectionMode
 import java.time.LocalDate
-import java.time.Month
 import java.time.YearMonth
 import java.time.format.TextStyle
 import java.util.*
@@ -51,13 +52,12 @@ fun CalendarScreen(
             initialSelection = state.value.checkedDates,
             initialSelectionMode = SelectionMode.Multiple
         )
-        // TODO add month header
         SelectableCalendar(
             modifier = Modifier
                 .testTag(stringResource(id = R.string.calendar))
                 .animateContentSize(),
             dayContent = { dayState ->
-                if(dayState.date.month == LocalDate.now().month){
+                if (dayState.date.month == LocalDate.now().month) {
                     DateBox(
                         RoundedCornerShape(2.dp),
                         dayState = dayState,
@@ -73,7 +73,11 @@ fun CalendarScreen(
             },
             calendarState = calendarState,
             monthHeader = { monthState ->
-                MonthHeader(monthState = monthState)
+                MonthHeader(
+                    monthState = monthState,
+                    checkedDateNum = state.value.checkedDates
+                        .filter { it.month == monthState.currentMonth.month }.size
+                )
             }
         )
     } else {
@@ -171,59 +175,115 @@ fun OtherMonthDateBox(
 fun MonthHeader(
     monthState: MonthState,
     modifier: Modifier = Modifier,
+    checkedDateNum: Int,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+    )
+    {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            androidx.compose.material.Text(
+                modifier = Modifier.testTag("MonthLabel"),
+                text = monthState.currentMonth.month
+                    .getDisplayName(TextStyle.FULL, Locale.getDefault())
+                    .lowercase()
+                    .replaceFirstChar { it.titlecase() },
+                style = MaterialTheme.typography.h4,
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            androidx.compose.material.Text(text = monthState.currentMonth.year.toString(),
+                style = MaterialTheme.typography.h4)
+        }
+
+        CheckedCounter(monthState = monthState, modifier = modifier, size = checkedDateNum)
+    }
+}
+
+@Composable
+fun CheckedCounter(
+    monthState: MonthState,
+    modifier: Modifier = Modifier,
+    size: Int
 ) {
     Row(
-        modifier = modifier.fillMaxWidth(),
+        modifier = Modifier.height(IntrinsicSize.Min),
         horizontalArrangement = Arrangement.Center,
     ) {
-        IconButton(
-            modifier = Modifier.testTag("Decrement"),
-            onClick = { monthState.currentMonth = monthState.currentMonth.minusMonths(1) }
-        ) {
-            Image(
-                imageVector = Icons.Default.KeyboardArrowLeft,
-                colorFilter = ColorFilter.tint(MaterialTheme.colors.onSurface),
-                contentDescription = "Previous",
-            )
-        }
-        androidx.compose.material.Text(
-            modifier = Modifier.testTag("MonthLabel"),
-            text = monthState.currentMonth.month
-                .getDisplayName(TextStyle.FULL, Locale.getDefault())
-                .lowercase()
-                .replaceFirstChar { it.titlecase() },
-            style = MaterialTheme.typography.h4,
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        androidx.compose.material.Text(text = monthState.currentMonth.year.toString(),
-            style = MaterialTheme.typography.h4)
-        // Show next button only if the month on the screen is not current month
-        if (monthState.currentMonth != YearMonth.now()) {
+        Row(modifier = Modifier.weight(0.25f)){
             IconButton(
-                modifier = Modifier.testTag("Increment"),
-                onClick = { monthState.currentMonth = monthState.currentMonth.plusMonths(1) }
+                modifier = modifier
+                    .testTag("Decrement")
+                    .weight(0.25f)
+                    .fillMaxSize()
+                ,
+                onClick = { monthState.currentMonth = monthState.currentMonth.minusMonths(1) }
             ) {
                 Image(
-                    imageVector = Icons.Default.KeyboardArrowRight,
+                    imageVector = Icons.Default.KeyboardArrowLeft,
                     colorFilter = ColorFilter.tint(MaterialTheme.colors.onSurface),
-                    contentDescription = "Next",
+                    contentDescription = "Previous",
                 )
+            }
+        }
+        Row(
+            modifier = Modifier
+            .weight(0.5f)
+            .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxHeight()
+                ,
+                painter = painterResource(id = R.drawable.ic_check_24),
+                contentDescription = "checked count",
+            )
+            //Text(text = "5 / ", modifier = Modifier.fillMaxSize(), fontSize = Typography.)
+            val checkedRatio = size.toString() + "f"
+            Text(
+                text = "$size/${monthState.currentMonth.lengthOfMonth()}",
+                fontSize = 24.sp
+            )
+        }
+        // Show next button only if the month on the screen is not current month
+        Row(modifier = Modifier.weight(0.25f)){
+            if (monthState.currentMonth != YearMonth.now()) {
+                IconButton(
+                    modifier = Modifier
+                        .testTag("Increment")
+                        .fillMaxSize()
+                    ,
+                    onClick = { monthState.currentMonth = monthState.currentMonth.plusMonths(1) }
+                ) {
+                    Image(
+                        imageVector = Icons.Default.KeyboardArrowRight,
+                        colorFilter = ColorFilter.tint(MaterialTheme.colors.onSurface),
+                        contentDescription = "Next",
+                    )
+                }
             }
         }
     }
 }
 
-//@Preview
-//@Composable
-//fun previewCalendar() {
-//    CalendarScreen(
-//        state = CalendarScreenState(
-//            listOf(),
-//            0,
-//            false,
-//        ),
-//        onCheckDate = {},
-//        onUncheckDate = {}
-//    )
-//}
+@Preview
+@Composable
+fun previewCalendar() {
+    CalendarScreen(
+        state = mutableStateOf(
+            CalendarScreenState(
+                listOf(),
+                0,
+                false,
+            )
+        ),
+        onCheckDate = {},
+        onUncheckDate = {}
+    )
+}
 
