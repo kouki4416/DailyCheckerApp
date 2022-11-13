5760e04deecb18aeb079a37cfb7dcc8ee5ba300b
kouki4416 <pank4416@gmail.com>
2022-11-13 15:36:25 +0900

    Added admob bottom bar

--- app/src/main/java/com/pyunku/dailychecker/MainScreen.kt
+++ app/src/main/java/com/pyunku/dailychecker/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.pyunku.dailychecker
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.*
 import androidx.compose.material3.*
@@ -8,11 +9,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import com.pyunku.dailychecker.navigation.Screen
 import com.pyunku.dailychecker.navigation.SetupNavGraph
 
@@ -22,7 +27,12 @@ import com.pyunku.dailychecker.navigation.SetupNavGraph
 fun MainScreen() {
     val navController = rememberNavController()
     Scaffold(
-        bottomBar = { BottomBar(navController = navController) }
+        bottomBar = {
+            Column {
+                BottomBar(navController = navController)
+                AdMobBar()
+            }
+        }
     ) {
         SetupNavGraph(navController = navController)
     }
@@ -34,7 +44,7 @@ fun BottomBar(navController: NavHostController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
-     NavigationBar {
+    NavigationBar {
         screens.forEach { screen ->
             AddItem(screen = screen,
                 currentDestination = currentDestination,
@@ -58,7 +68,7 @@ fun RowScope.AddItem(
             Icon(
                 painter = painterResource(screen.icon),
                 contentDescription = "Navigation Icon",
-                tint = if(selected) Color.White else Color.DarkGray.copy(alpha = 0.5f)
+                tint = if (selected) Color.White else Color.DarkGray.copy(alpha = 0.5f)
             )
         },
         label = {
@@ -69,4 +79,17 @@ fun RowScope.AddItem(
         },
         //unselectedContentColor = LocalContentColor.current.copy(alpha = 0f)
     )
+}
+
+@Composable
+fun AdMobBar() {
+    AndroidView(factory = {
+        AdView(it).apply {
+            setAdSize(AdSize.BANNER)
+            //adUnitId = "ca-app-pub-7719172067804321/1743343072"
+            //Test
+            adUnitId = "ca-app-pub-3940256099942544/6300978111"
+            loadAd(AdRequest.Builder().build())
+        }
+    })
 }
